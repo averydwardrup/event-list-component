4089b3e8286b580c42f05eb52204545e7f95fc3b
Avery Wardrup <avery.wardrup4@gmail.com>
2023-05-09 23:17:26 -0400

    added vue component and readme

---
+++ EventsList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="d-flex g-5">
+            <div 
+                class="card p-0 col-md-4"
+                v-for="{ id, title, excerpt } in events"
+                :key="id"
+            >
+                <img src="https://cdn.pixabay.com/photo/2023/04/25/15/08/church-7950418_960_720.jpg" class="card-img-top" :alt="title">
+                <div class="card-body">
+                    <h3 class="mt-3">{{ title }}</h3>
+                    <div v-html="excerpt" class="card-text"></div>
+                </div>
+            </div>
+        </div>  
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: "EventsList",
+    data() {
+        return {
+            events: undefined,
+            time: undefined
+        }
+    },
+    mounted() {
+        this.fetchEvents()
+    },
+    methods: {
+        async fetchEvents() {
+            try {
+                const res = await axios.get('YOUR_URL/tribe/events/v1/events')
+                this.events = res.data.events
+            } catch (error ) {
+                console.log(error)
+            }
+        }
+    }
+}
+</script>
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# event-list-component
+# Vue 3 Event List Component
 Vue 3 event list component pulling events from The Events Calendar rest API
+
+## Dev Dependencies
++ [Axios](https://www.npmjs.com/package/axios)
++ [Bootstrap 5](https://www.npmjs.com/package/bootstrap/v/5.0.1)
+
+## Notes
++ v-html should only be used in situations where the data is sanitized and cleaned beforehand
++ The Events Calendar [API Documentation](https://theeventscalendar.com/knowledgebase/k/introduction-to-the-events-calendar-rest-api/)
+
+## Usage
+
+### Import into your view
+
+````bash
+import ToolCards from '../components/EventsList.vue'
+````
+
+### Define your component
+
+````bash
+components: {
+    ToolCards
+}
+````
+
+### Add your component within the template
+
+````bash
+<EventsList />
+````
